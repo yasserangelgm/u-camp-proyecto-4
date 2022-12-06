149e9924c8a8e16e25b7ed25fe017e7aa26e3f30
Yasser Angel <yasserangelgm@gmail.com>
2022-12-05 21:34:51 -0600

    Add items to menu category

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,15 +1,18 @@
 import './menu.styles.css';
 
 import { db } from '../../config/firebase';
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
-import { useState, useEffect, cloneElement } from 'react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
 
 const Menu = () => {
   const [menus, setMenus] = useState([]);
-  const [menuItems, setMenuItems] = useState([]);
+  const [items, setItems] = useState([]);
+  // const [menuItems, setMenuItems] = useState([]);
 
   useEffect(() => {
-    const menuQuery = query(collection(db, 'menu'));
+    const menuQuery = query(collection(db, 'menu'), orderBy('order'));
+    const itemQuery = query(collection(db, 'menuitems'));
+
     onSnapshot(menuQuery, (querySnapshot) => {
       setMenus(
         querySnapshot.docs.map((menu) => {
@@ -17,10 +20,26 @@ const Menu = () => {
         })
       );
     });
+
+    onSnapshot(itemQuery, (querySnapshot) => {
+      setItems(
+        querySnapshot.docs.map((item) => {
+          return { id: item.id, data: item.data() };
+        })
+      );
+    });
   }, []);
-  //TODO: cargar los sub-elementos de cada menu en caso de existir (No se muy como modelar maestro-detalle en FireBase)
-  /*     const itemsQuery = query(collection(db, 'menu'));
-   */
+
+  /*   const renderItems = (menuId) => {
+    return items
+      .filter((item) => item.data.menuId === menuId)
+      .map((menuItem) => (
+        <>
+          <p className="item-name">{menuItem.data.title}</p>
+          <p className="item-desc">{menuItem.data.description}</p>
+        </>
+      ));
+  }; */
 
   return (
     <>
@@ -36,6 +55,14 @@ const Menu = () => {
                 <div className="category-wrapper">
                   <p className="item-name">{menu.data.optionDescription}</p>
                   <p className="item-desc">{menu.data.options}</p>
+                  {items
+                    .filter((item) => item.data.menuId === menu.id)
+                    .map((menuItem, index) => (
+                      <div key={index}>
+                        <p className="item-name">{menuItem.data.title}</p>
+                        <p className="item-desc">{menuItem.data.description}</p>
+                      </div>
+                    ))}
                 </div>
               </div>
             </div>
